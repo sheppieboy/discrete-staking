eb4c97a15959d76ed39ee1c72e42298e45841973
Luke Sheppard <sheppiegit@gmail.com>
2023-08-10 16:53:27 -0400

    added constructor and interface of ERC20.

--- src/DiscreteStakingRewards.sol
+++ src/DiscreteStakingRewards.sol
@@ -1,4 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract DiscreteStakingRewards {}
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+}
+
+contract DiscreteStakingRewards {
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardToken;
+
+    constructor(address _stakingToken, address _rewardToken) {
+        stakingToken = IERC20(_stakingToken);
+        rewardToken = IERC20(_rewardToken);
+    }
+}
